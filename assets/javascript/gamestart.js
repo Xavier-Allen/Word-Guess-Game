57d4d3e0a6ab4956033077575bb814f838e9bcd6
unknown <xavierallen@email.arizona.edu>
2018-08-09 18:32:52 -0700

    changed some JS

--- assets/javascript/gamestart.js
+++ assets/javascript/gamestart.js
@@ -1,16 +1,35 @@
+var dallas = {
+    "answers": {
+        "dirk" : "dirk nowizki",
+        "tony" : "tony romo",
+        "roger" : "roger staubach",
+        "irvin": "michael irvin",
+        "jason" : "jason terry"
+    },
+    "function": function game(){
+        
+    }
+};
+    
+
+
 // storing a function inside "random" that returns the value of a random key from an object.
-var random = function ranObject (obj){
+function ranObject (obj){
 
     // setting "something" to equal the keys of "answers" of the object. 
-    var something = object.keys(obj.answers);
+    var something = Object.keys(obj.answers);
 
     // setting "guess" to randomize the order of the keys
     var guess = Math.floor(Math.random() * something.length);
 
     // get the first key from the now randomized keys
-    return guess[0];
+    return obj.answers[something[guess]];
 }
+// creates a variable that returns a random value like "dirk nowizki or tony romo"
+var thing = ranObject(dallas);
+console.log(thing);
+var spaces = "";
 
-for (var i = 0; i < random.length; i++){
-    
+for (var i = 0; i < thing.length; i++){
+    spaces += "_";
 }
